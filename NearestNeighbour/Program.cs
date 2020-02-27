da8b31f1da12353dc7e83ed7e802d32e2324d5d8
skiassat@basemap.co.uk <skiassat@basemap.co.uk>
2020-02-27 10:51:14 +0000

    Add project files.

---
+++ NearestNeighbour/Program.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearestNeighbour
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("Origin.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Point>().ToList();
+
+            Console.WriteLine("Carmen do the magic - safe");
+            Console.ReadKey();
+            }
+
+        }
+    }
+}
